36259f1e64423df7831f5d258c9cf2d3e1923b94
Daniel <30957352+derstolz@users.noreply.github.com>
2019-06-15 07:30:35 -0400

    Update run-backend.sh

--- run-backend.sh
+++ run-backend.sh
@@ -6,5 +6,4 @@ database_name=cameras_db
 database_user=rtsp
 database_password=changeme3
 
-cd ./java && nohup mvn spring-boot:run -Dserver.port=$port -Dserver.address=$host -Dspring.datasource.username=$database_user -Dspring.datasource.password=$database_password -Dspring.datasource.url=jdbc:mariadb://localhost:3306/$database_name &
-disown
+cd ./java && mvn spring-boot:run -Dserver.port=$port -Dserver.address=$host -Dspring.datasource.username=$database_user -Dspring.datasource.password=$database_password -Dspring.datasource.url=jdbc:mariadb://localhost:3306/$database_name
