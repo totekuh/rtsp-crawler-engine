90f2af9bb170d277545dc9c1ab01e5ab1ebec107
retr0 <downtownxore@gmail.com>
2019-06-14 22:47:31 -0400

    add database credentials to the start script

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 
 apt install python3 python3-pip java mvn mariadb tor
 pip3 install requests flask opencv-python
+
+# db initialization:
+CREATE DATABASE cameras_db;
+CREATE USER rtsp@localhost IDENTIFIED BY 'changeme3';
+GRANT ALL PRIVILEGES ON cameras_db.* TO rtsp@localhost IDENTIFIED BY 'changeme3';
