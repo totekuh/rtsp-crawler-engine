eb8ffa75e81ad3777ed2074a6c39929333955d95
retr0 <downtownxore@gmail.com>
2019-06-14 22:45:47 -0400

    add database credentials to the start script

--- run-backend.sh
+++ run-backend.sh
@@ -2,5 +2,8 @@
 
 port=80
 host=localhost
+database_name=cameras_db
+database_user=rtsp
+database_password=changeme3
 
-cd ./java && mvn spring-boot:run -Dserver.port=$port -Dserver.address=$host
+cd ./java && mvn spring-boot:run -Dserver.port=$port -Dserver.address=$host -Dspring.datasource.username=$database_user -Dspring.datasource.password=$database_password -Dspring.datasource.url=jdbc:mariadb://localhost:3306/$database_name
