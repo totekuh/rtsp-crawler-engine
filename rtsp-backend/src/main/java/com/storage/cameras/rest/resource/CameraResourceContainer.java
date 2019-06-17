f71b170c8ffb2ad11b7ac35b439d82c5e8623efc
retr0 <downtownxore@gmail.com>
2019-06-17 11:39:14 -0400

    add validation for the camera resource class

--- rtsp-backend/src/main/java/com/storage/cameras/rest/resource/CameraResourceContainer.java
+++ rtsp-backend/src/main/java/com/storage/cameras/rest/resource/CameraResourceContainer.java
@@ -1,13 +1,19 @@
 package com.storage.cameras.rest.resource;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class CameraResourceContainer {
+import static java.util.Collections.emptyList;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 
-    private List<CameraResource> cameras;
+@Getter
+public class CameraResourceContainer {
+    private final int totalCount;
+    private final List<CameraResource> cameras;
+
+    public CameraResourceContainer(final List<CameraResource> cameras) {
+        this.cameras = isNotEmpty(cameras) ? cameras : emptyList();
+        this.totalCount = isNotEmpty(cameras) ? cameras.size() : 0;
+    }
 }
