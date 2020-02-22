687419222c6d6a01a0b3d6e477ee4d6f8522c67e
retr0 <you@example.com>
2020-02-22 18:55:41 -0500

    working on many to many

--- run-backend.sh
+++ run-backend.sh
@@ -4,6 +4,6 @@ port=8080
 host=localhost
 database_name=cameras_db
 database_user=rtsp
-database_password=TGV0cyBmdWNrIHRpbGwgdGhlIHN1biBnb2VzIGRvd24K
+database_password=changeme3
 
 cd ./rtsp-backend && mvn spring-boot:run -Dserver.port=$port -Dserver.address=$host -Dspring.datasource.username=$database_user -Dspring.datasource.password=$database_password -Dspring.datasource.url=jdbc:mariadb://localhost:3306/$database_name
