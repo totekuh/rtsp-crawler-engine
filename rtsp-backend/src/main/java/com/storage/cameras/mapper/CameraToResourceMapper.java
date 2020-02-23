1e0322f757d8c22deefaa734331e1064a2917246
retr0 <you@example.com>
2020-02-22 19:29:38 -0500

    working on many to many

--- rtsp-backend/src/main/java/com/storage/cameras/mapper/CameraToResourceMapper.java
+++ rtsp-backend/src/main/java/com/storage/cameras/mapper/CameraToResourceMapper.java
@@ -4,6 +4,7 @@ import com.storage.cameras.model.Camera;
 import com.storage.cameras.rest.resource.CameraResource;
 import static com.storage.cameras.util.DateTimeUtil.formatDateToString;
 import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
 public interface CameraToResourceMapper {
     CameraToResourceMapper INSTANCE = camera -> new CameraResource(
@@ -27,7 +28,7 @@ public interface CameraToResourceMapper {
 
     default CameraResource convert(Camera camera, Boolean includeBase64ImageData) {
         final CameraResource cameraResource = INSTANCE.convert(camera);
-        if (includeBase64ImageData) {
+        if (isTrue(includeBase64ImageData)) {
             cameraResource.setBase64ImageData(null);
         }
         return cameraResource;
